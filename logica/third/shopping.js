32a19be9f54076b3a7fb4ec25f4604b412239fe3
Felipe <felipe_oliveira2019@hotmail.com>
2025-06-26 18:27:07 -0300

    the 3 challenge

---
+++ logica/third/shopping.js
@@ -0,0 +1,11 @@
+const produtos = []
+
+
+
+costumer = [
+    { name: "pc", price: 5000, quantity: 1 },
+    { name: "keyboard", price: 250, quantity: 3 },
+    { name: "mouse", price: 100, quantity: 2 },
+    { name: "pc", price: 5000, quantity: 1 }
+
+]
