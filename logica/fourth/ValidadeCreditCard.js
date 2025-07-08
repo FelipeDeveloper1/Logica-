f599dec3b0c9499fff32201350ed344980c03655
Felipe <felipe_oliveira2019@hotmail.com>
2025-07-08 20:48:44 -0300

    parse string numbers to int numbers

--- logica/fourth/ValidadeCreditCard.js
+++ logica/fourth/ValidadeCreditCard.js
@@ -2,19 +2,15 @@ function ValidCreditCard(digits) {
 
     const cardNumber = digits.toString().split('')
     let finalNumber = []
-    finalNumber = cardNumber.map((num) => num * 2 > 9 ? num * 2 - 9 : num)
+    finalNumber = cardNumber.map((num) => num * 2 > 9 ? num * 2 - 9 : parseInt(num))
     console.log(finalNumber)
-    const totalSum = finalNumber.reduce((accumulator, element) => {
-        return accumulator + element
-    })
-    console.log(totalSum)
-    const nums = [1, '5', 10, '10']
-    let total = nums.reduce((accumulator, element) => {
-        return accumulator + element
-    })
-    console.log(total)
-}
 
+    const sum = (accumulator, element) => {
+        // return accumulator + element
+    }
+    let total = finalNumber.reduce(sum)
+        // console.log(total)
+}
 
 
 ValidCreditCard(4539578763621486)
