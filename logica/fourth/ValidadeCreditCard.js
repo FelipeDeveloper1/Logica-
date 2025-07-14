1d61177689d9154e90687c45c211ee055ee59d87
Felipe <felipe_oliveira2019@hotmail.com>
2025-07-14 20:30:50 -0300

    fixing some issues in the feature

--- logica/fourth/ValidadeCreditCard.js
+++ logica/fourth/ValidadeCreditCard.js
@@ -1,16 +1,50 @@
-function ValidCreditCard(digits) {
+// function ValidCreditCard(digits) {
+//     const cardNumber = digits.toString().split('')
+//     let finalNumber = []
+//     finalNumber = cardNumber.map((num) => num * 2 > 9 ? num * 2 - 9 : parseInt(num))
+//     let totalValue = finalNumber.reduce((accumulator, element) => {
+//         return accumulator + element
+//     })
+//     console.log(totalValue)
+//     if (totalValue % 10 == 0) {
+//         console.log("valid card")
+//     } else {
+//         console.log("invalid card")
+//     }
 
-    const cardNumber = digits.toString().split('')
-    let finalNumber = []
-    finalNumber = cardNumber.map((num) => num * 2 > 9 ? num * 2 - 9 : parseInt(num))
-    console.log(finalNumber)
 
-    const sum = (accumulator, element) => {
-        // return accumulator + element
+// }
+
+
+// ValidCreditCard(1234567812345670)
+
+
+
+// correct form 
+function creditCard(card) {
+    // format to string
+
+    let cardnumber = card.toString().split('').reverse()
+    let doublingElement = cardnumber.map((num, index) => {
+        if (index % 2 == 1) {
+            return num * 2 > 9 ? num * 2 - 9 : num * 2
+        } else {
+            return parseInt(num)
+        }
+    })
+    let finalcard = doublingElement.reduce((accumulator, element) => {
+        return accumulator + element
+    })
+    const validation = () => {
+        return finalcard % 10 == 0 ? "valid card" : "invalid card"
     }
-    let total = finalNumber.reduce(sum)
-        // console.log(total)
+    console.log(finalcard)
+
+    console.log(validation())
 }
 
 
-ValidCreditCard(4539578763621486)
+
+
+
+creditCard(4539578763621486)
