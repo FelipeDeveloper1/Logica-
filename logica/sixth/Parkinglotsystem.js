41da73075d2beb6df836143e1aed337319d827b2
Felipe <felipe_oliveira2019@hotmail.com>
2025-08-04 21:04:10 -0300

    return the price based on the time spent

--- logica/sixth/Parkinglotsystem.js
+++ logica/sixth/Parkinglotsystem.js
@@ -1,19 +1,45 @@
+const hour = new Date()
+
+
 const parkingLot = {
-    spots: 20,
-    occupiedSpots: 0,
-    perhour: 10
+    spots: 5,
+    perhour: 10,
+    time: hour.getHours()
 }
 
 class ValidateParkentry {
     constructor(plate, type, entrytime) {
         if (parkingLot.spots > 0) {
-            this.carDescription = { plate, type, entrytime }
+            this.carDescription = { plate, type, entrytime, pending: "none" }
             parkingLot.spots -= 1
-            parkingLot.perhour += 2
+            this.carDescription.status = "in"
+            console.log(this.carDescription)
+
+        } else {
+            console.log("there's no much spots for now")
         }
+
+        this.leave = () => {
+            if (this.carDescription.status == "out") {
+                console.log("you already outside")
+
+            } else {
+                parkingLot.spots += 1
+                this.carDescription.status = "out"
+                const timing = parkingLot.time -= this.carDescription.entrytime
+                this.carDescription.pending = timing * parkingLot.perhour
+                console.log(`you need pay: ${this.carDescription.pending} dolars`)
+            }
+        }
+
     }
 
 }
 
-
-const Car = new ValidateParkentry(1234, "onix", entryme)
+const entry = () => {
+    return Math.floor(Math.random() * hour.getHours())
+}
+const car1 = new ValidateParkentry(1234, "onix", entry())
+    // car1.leave()
+const car2 = new ValidateParkentry(203, "ferrari", entry())
+car2.leave()
